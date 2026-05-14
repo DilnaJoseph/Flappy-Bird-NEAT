4aabb54e71e50452010213cd03d8e8ba256f9826
DilnaJoseph <dilnamjoseph@gmail.com>
2026-05-14 22:09:09 +0530

    Add NodeGene class for genome neuron representation

---
+++ neat/node_gene.py
@@ -0,0 +1,39 @@
+"""
+the node_gene is a neuron in the genome 
+only genetic information is stored in this file 
+"""
+
+from copy import deepcopy
+
+class NodeGene:
+    INPUT="INPUT"
+    HIDDEN="HIDDEN"
+    OUTPUT="OUTPUT"
+    BIAS="BIAS"
+  
+    VALID_NODE_TYPES={
+      INPUT,HIDDEN,OUTPUT,BIAS
+    }
+  
+    def __init__(self,node_id: int,node_type: str,layer: float=0.0):
+      if node_type not in self.VALID_NODE_TYPES:
+        raise ValueError(f"Invalid node type: {node_type}")
+      
+      self.id = node_id
+      self.type = node_type
+      self.layer = layer
+    
+    def copy(self):
+      return deepcopy(self)
+    
+    def __repr__(self):
+      return (f"NodeGene("f"id={self.id},"f"type='{self.type}',"f"layer={self.layer}"f")" )
+      
+    def __eq__(self,other):
+      if not isinstance(other,NodeGene):
+        return False
+        
+      return self.id == other.id
+    
+    def __hash__(self):
+      return hash(self.id)
